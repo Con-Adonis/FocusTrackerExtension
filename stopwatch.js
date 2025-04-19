48caa0e19e616c6f00ce20a2d4455513505c207e
Con-Adonis <conkavleski@gmail.com>
2025-04-18 21:16:05 -0400

    fixed timer desync

--- stopwatch.js
+++ stopwatch.js
@@ -1,5 +1,25 @@
-document.getElementById("myButton").addEventListener("click", myFunction);
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById("startButton");
+  
+    startButton.addEventListener("click", function () {
+      let seconds = 0, minutes = 0, hours = 0;
+      const timerDisplay = document.getElementById("displayTime");
 
-function myFunction(){
-  console.log('asd');
-}
+      timerDisplay.textContent = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+      setInterval(() => {
+        seconds++;
+        timerDisplay.textContent = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+        if (seconds == 60){
+            minutes++
+            seconds = 0
+        }
+        if (minutes == 60){
+            hours++
+            minutes = 0
+        }
+      }, 1000);
+    });
+  });
+  
